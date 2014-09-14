37d21b4c7e585ca8bb3866e94713006a74a52d83
Ian Blenke <ian@blenke.com>
2014-09-13 21:25:50 -0400

    Reworked systemd service dependencies, panamax is finally running

--- start.sh
+++ start.sh
@@ -5,8 +5,8 @@ COREOS_CPUS=${COREOS_CPUS:-1}
 COREOS_CHANNEL=${COREOS_CHANNEL:-alpha}
 NUM_INSTANCES=${NUM_INSTANCES:-3}
 
-ENABLE_KUBERNETES=${ENABLE_KUBERNETS:-false}
-ENABLE_LIBSWARM=${ENABLE_LIBSWARM:-true}
+ENABLE_KUBERNETES=${ENABLE_KUBERNETS:-true}
+ENABLE_LIBSWARM=${ENABLE_LIBSWARM:-false}
 ENABLE_PANAMAX=${ENABLE_PANAMAX:-false}
 
 export COREOS_MEMORY COREOS_CPUS COREOS_CHANNEL NUM_INSTANCES
@@ -22,7 +22,6 @@ cat user-data.sample rudder.yml > user-data
 
 [ "$ENABLE_KUBERNETES" = "true" ] && cat kubernetes-minion.yml >> user-data
 [ "$ENABLE_LIBSWARM" = "true" ] && cat libswarm.yml >> user-data
-[ "$ENABLE_PANAMAX" = "true" ] && cat panamax.yml >> user-data
 
 vagrant up
 
@@ -30,7 +29,11 @@ NODES=( $(vagrant status | grep running | awk '{print $1}') )
 MASTER=${NODES[0]}
 
 [ "$ENABLE_KUBERNETES" = "true" ] && (
-  cat kubernetes-master.yml | vagrant ssh $MASTER -c "cat - > master.yml ; sudo /usr/bin/coreos-cloudinit --from-file=/home/core/kubernetes-master.yml"
+  cat kubernetes-master.yml | vagrant ssh $MASTER -c "cat - > kubernetes-master.yml ; sudo /usr/bin/coreos-cloudinit --from-file=/home/core/kubernetes-master.yml"
+)
+
+[ "$ENABLE_PANAMAX" = "true" ] && (
+  cat panamax-master.yml | vagrant ssh $MASTER -c "cat - > panamax-master.yml ; sudo /usr/bin/coreos-cloudinit --from-file=/home/core/panamax-master.yml"
 )
 
 echo "Master: $MASTER"
