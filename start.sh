b378b5c80fa073cea1721ac8972a403b58d93c24
Ian Blenke <ian@blenke.com>
2014-09-29 04:28:13 +0000

    Adding elasticsearch/logstash/kibana

--- start.sh
+++ start.sh
@@ -9,10 +9,12 @@ ENABLE_KUBERNETES=${ENABLE_KUBERNETS:-false}
 ENABLE_LIBSWARM=${ENABLE_LIBSWARM:-false}
 ENABLE_PANAMAX=${ENABLE_PANAMAX:-false}
 ENABLE_ELASTICSEARCH=${ENABLE_ELASTICSEARCH:-true}
+ENABLE_KIBANA=${ENABLE_KIBANA:-true}
+ENABLE_LOGSTASH=${ENABLE_LOGSTASH:-true}
 
 export COREOS_MEMORY COREOS_CPUS COREOS_CHANNEL NUM_INSTANCES
 
-set -e
+set -ex
 
 which vagrant || (
   echo "Vagrant is required for this project"
@@ -21,15 +23,20 @@ which vagrant || (
 
 (
   cat user-data.sample
-  cat cloud-init/flannel-route-manager.unit
+  cat cloud-init/flannel.unit
   [ "$ENABLE_LIBSWARM" = "true" ] && cat cloud-init/libswarm.unit
   [ "$ENABLE_KUBERNETES" = "true" ] && cat cloud-init/kubernetes.unit
   [ "$ENABLE_ELASTICSEARCH" = "true" ] && cat cloud-init/elasticsearch.unit
+  [ "$ENABLE_LOGSTASH" = "true" ] && cat cloud-init/logstash.unit
+  [ "$ENABLE_KIBANA" = "true" ] && cat cloud-init/kibana.unit
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.unit
   echo "write_files:"
   [ "$ENABLE_KUBERNETES" = "true" ] && cat cloud-init/kubernetes.write_file
   [ "$ENABLE_ELASTICSEARCH" = "true" ] && cat cloud-init/elasticsearch.write_file
+  [ "$ENABLE_LOGSTASH" = "true" ] && cat cloud-init/logstash.write_file
+  [ "$ENABLE_KIBANA" = "true" ] && cat cloud-init/kibana.write_file
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.write_file
 ) > user-data
 
 vagrant up
+
