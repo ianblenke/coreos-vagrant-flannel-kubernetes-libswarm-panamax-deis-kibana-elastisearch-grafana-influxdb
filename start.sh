50dcb3d86b1e1095d8e46cc37dcc95416786530d
Ian Blenke <ian@blenke.com>
2014-09-30 02:07:40 +0000

    Adding deis, fixing conditions

--- start.sh
+++ start.sh
@@ -11,6 +11,7 @@ ENABLE_PANAMAX=${ENABLE_PANAMAX:-true}
 ENABLE_ELASTICSEARCH=${ENABLE_ELASTICSEARCH:-true}
 ENABLE_KIBANA=${ENABLE_KIBANA:-true}
 ENABLE_LOGSTASH=${ENABLE_LOGSTASH:-true}
+ENABLE_DEIS=${ENABLE_DEIS:-true}
 
 export COREOS_MEMORY COREOS_CPUS COREOS_CHANNEL NUM_INSTANCES
 
@@ -30,12 +31,14 @@ which vagrant || (
   [ "$ENABLE_LOGSTASH" = "true" ] && cat cloud-init/logstash.unit
   [ "$ENABLE_KIBANA" = "true" ] && cat cloud-init/kibana.unit
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.unit
+  [ "$ENABLE_DEIS" = "true" ] && cat cloud-init/deis.unit
   echo "write_files:"
   [ "$ENABLE_KUBERNETES" = "true" ] && cat cloud-init/kubernetes.write_file
   [ "$ENABLE_ELASTICSEARCH" = "true" ] && cat cloud-init/elasticsearch.write_file
   [ "$ENABLE_LOGSTASH" = "true" ] && cat cloud-init/logstash.write_file
   [ "$ENABLE_KIBANA" = "true" ] && cat cloud-init/kibana.write_file
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.write_file
+  [ "$ENABLE_DEIS" = "true" ] && cat cloud-init/deis.write_file
 ) > user-data
 
 vagrant up
