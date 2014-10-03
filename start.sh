cd5a87d34e2519a08ec6e464b78c8b2e0e4335e5
Ian Blenke <ian@blenke.com>
2014-10-03 06:41:50 +0000

    Adding influxdb, cadvisor, grafana, and heapster

--- start.sh
+++ start.sh
@@ -16,7 +16,17 @@ ENABLE_LOGSTASH=${ENABLE_LOGSTASH:-false}
 
 ENABLE_DEIS=${ENABLE_DEIS:-false}
 
-ENABLE_ZOOKEPER=${ENABLE_ZOOKEPER:-false}
+ENABLE_ZOOKEEPER=${ENABLE_ZOOKEEPER:-false}
+
+ENABLE_INFLUXDB=${ENABLE_INFLUXDB:-false}
+ENABLE_CADVISOR=${ENABLE_CADVISOR:-false}
+ENABLE_GRAFANA=${ENABLE_GRAFANA:-false}
+ENABLE_HEAPSTER=${ENABLE_HEAPSTER:-false}
+
+[ "${ENABLE_HEAPSTER}" = "true" ] && ENABLE_GRAFANA=true
+[ "${ENABLE_GRAFANA}" = "true" ] && ENABLE_CADVISOR=true
+[ "${ENABLE_GRAFANA}" = "true" ] && ENABLE_ELASTICSEARCH=true
+[ "${ENABLE_CADVISOR}" = "true" ] && ENABLE_INFLUXDB=true
 
 export COREOS_MEMORY COREOS_CPUS COREOS_CHANNEL NUM_INSTANCES
 
@@ -38,6 +48,10 @@ which vagrant || (
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.unit
   [ "$ENABLE_DEIS" = "true" ] && cat cloud-init/deis.unit
   [ "$ENABLE_ZOOKEEPER" = "true" ] && cat cloud-init/zookeeper.unit
+  [ "$ENABLE_INFLUXDB" = "true" ] && cat cloud-init/influxdb.unit
+  [ "$ENABLE_CADVISOR" = "true" ] && cat cloud-init/cadvisor.unit
+  [ "$ENABLE_GRAFANA" = "true" ] && cat cloud-init/grafana.unit
+  [ "$ENABLE_HEAPSTER" = "true" ] && cat cloud-init/heapster.unit
   echo "write_files:"
   [ "$ENABLE_KUBERNETES" = "true" ] && cat cloud-init/kubernetes.write_file
   [ "$ENABLE_ELASTICSEARCH" = "true" ] && cat cloud-init/elasticsearch.write_file
@@ -46,6 +60,11 @@ which vagrant || (
   [ "$ENABLE_PANAMAX" = "true" ] && cat cloud-init/panamax.write_file
   [ "$ENABLE_DEIS" = "true" ] && cat cloud-init/deis.write_file
   [ "$ENABLE_ZOOKEEPER" = "true" ] && cat cloud-init/zookeeper.write_file
+  [ "$ENABLE_INFLUXDB" = "true" ] && cat cloud-init/influxdb.write_file
+  [ "$ENABLE_CADVISOR" = "true" ] && cat cloud-init/cadvisor.write_file
+  [ "$ENABLE_GRAFANA" = "true" ] && cat cloud-init/grafana.write_file
+  [ "$ENABLE_HEAPSTER" = "true" ] && cat cloud-init/heapster.write_file
+  true
 ) > user-data
 
 vagrant up
