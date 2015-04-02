9ec2262a1522507864b8a0a9780564cf9a1e2fa1
Ian Blenke <ian@blenke.com>
2015-04-02 11:58:09 -0400

    Altering converge script to remove the swap and add the users cloud-init

--- tested/converge.sh
+++ tested/converge.sh
@@ -2,10 +2,10 @@
 cd /var/lib/deis/store/cloud-init
 sudo coreos-cloudinit --from-file=ec2.cloud-init
 sudo coreos-cloudinit --from-file=ntp.cloud-init
-sudo coreos-cloudinit --from-file=swap.cloud-init
 sudo coreos-cloudinit --from-file=blackhole.cloud-init
 sudo coreos-cloudinit --from-file=newrelic.cloud-init
 sudo coreos-cloudinit --from-file=elasticsearch.cloud-init
 sudo coreos-cloudinit --from-file=fluentd.cloud-init
 sudo coreos-cloudinit --from-file=packetbeat.cloud-init
 sudo coreos-cloudinit --from-file=statsd-librato.cloud-init
+sudo coreos-cloudinit --from-file=users.cloud-init
